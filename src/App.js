e24b64b0cf2176d8a96f73f5fa18aa55eca94353
Tenzin <tenthinten@gmail.com>
2022-01-28 18:11:09 +0530

    use usestate for height and weight

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  
+  const[height,setHeight] = useState(null);
+  const[weight,setWeight] = useState(null);
+  const [status,setStatus] = useState('')
+
+
+
   return (
-    <div class="w-full max-w-xs">
+    <div class="w-full max-w-xs m-10">
   <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <div class="mb-4">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
         Height
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Height" type="text" placeholder="Height" />
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Height in cm" type="text" placeholder="Height" />
     </div>
     <div class="mb-6">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
         Weight
       </label>
-      <input class="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="Weight" type="password" placeholder="Weight" />
+      <input class="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="Weight" type="weight in kg" placeholder="Weight" />
     </div>
-    <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+    <div class="flex items-center justify-center">
+      <button class="bg-purple-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
         Calculate BMI
       </button>
     </div>
@@ -26,4 +34,3 @@ function App() {
   );
 }
 
-export default App;
